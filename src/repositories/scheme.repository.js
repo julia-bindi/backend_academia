9c3d138661cc3f545edd26e919d029206590c01c
julia-bindi <juliabindi@gmail.com>
2022-06-30 07:42:40 -0300

    add getAll to scheme repository

--- src/repositories/scheme.repository.js
+++ src/repositories/scheme.repository.js
@@ -4,6 +4,7 @@ module.exports = {
   list: (query) => Scheme.findAndCountAll(query),
   getById: (id) => Scheme.findByPk(id),
   get: (params) => Scheme.findOne({ where: params }),
+  getAll: () => Scheme.findAll(),
   create: (params) => Scheme.create(params),
   update: (scheme) => scheme.save(),
   destroy: (id) => Scheme.destroy({ where: { id } }),
